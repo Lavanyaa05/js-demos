6ba4c663088234fb9182918b31a36bccaf21a051
Lavanyaa05 <mnvnnnlavanya@gmail.com>
2026-07-21 17:20:14 +0800

    Add profile bio and statistics

--- github-profile-finder/script.js
+++ github-profile-finder/script.js
@@ -26,8 +26,8 @@ searchForm.addEventListener("submit", function (event) {
     })
     .then(function (user) {
       console.log(user);
-      showBasicProfile(user);
 
+      showProfile(user);
       profileMessage.textContent = "Profile loaded.";
     })
     .catch(function (error) {
@@ -38,7 +38,7 @@ searchForm.addEventListener("submit", function (event) {
     });
 });
 
-function showBasicProfile(user) {
+function showProfile(user) {
   const profileImage = document.createElement("img");
   profileImage.src = user.avatar_url;
   profileImage.alt = user.login + " profile picture";
@@ -57,7 +57,32 @@ function showBasicProfile(user) {
   const profileUsername = document.createElement("p");
   profileUsername.textContent = "@" + user.login;
 
+  const profileBio = document.createElement("p");
+
+  if (user.bio) {
+    profileBio.textContent = user.bio;
+  } else {
+    profileBio.textContent = "No bio available.";
+  }
+
+  const profileStats = document.createElement("div");
+
+  const repoCount = document.createElement("p");
+  repoCount.textContent = "Public repositories: " + user.public_repos;
+
+  const followerCount = document.createElement("p");
+  followerCount.textContent = "Followers: " + user.followers;
+
+  const followingCount = document.createElement("p");
+  followingCount.textContent = "Following: " + user.following;
+
+  profileStats.appendChild(repoCount);
+  profileStats.appendChild(followerCount);
+  profileStats.appendChild(followingCount);
+
   profileResult.appendChild(profileImage);
   profileResult.appendChild(profileName);
   profileResult.appendChild(profileUsername);
+  profileResult.appendChild(profileBio);
+  profileResult.appendChild(profileStats);
 }
